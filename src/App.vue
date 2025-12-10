8b7843ed5c9926e6cb5659f34201cc98c7f14328
zhaojingnan <2026617199@qq.com>
2025-12-10 09:54:43 +0800

    初始化前端项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+#app {
+  min-height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
